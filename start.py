10480a75904f55afad5e154b4dc6508a8dfa0bc2
K1rLes <104463209+K1rL3s@users.noreply.github.com>
2022-05-22 00:05:50 +0500

    Update start.py

--- start.py
+++ start.py
@@ -15,8 +15,12 @@ chatlink = input('Chat link where bot will work >>> ')
 print(f'{client.get_from_code(code=chatlink).objectId} - paste it in READ_CHATS in db.py')
 reportlink = input('Chat link where bot will send reports >>> ')
 print(f"'{client.get_from_code(code=reportlink).objectId}' - paste it in READ_CHATS in db.py\n")
-print('if you want send report to the dm or private chat, use it:\n\n'
-      'chats_info = sub_client.get_chat_threads(start=0, size=100)\n'
-      'for name, chat_id in zip(chats_info.title, chats_info.chatId):\n'
-      '----print(name, chat_id)\n')
+print("if you want send report to the dm or private chat, use it:\n\n"
+      "import aminofix as amino\n"
+      "client = amino.Client()\n"
+      "client.login(email=EMAIL, password=PASSWORD)\n"
+      "sub_client = amino.SubClient(comId=CID, profile=client.profile)\n"
+      "chats_info = sub_client.get_chat_threads(start=0, size=100)\n"
+      "for name, chat_id in zip(chats_info.title, chats_info.chatId):\n"
+      "----print(name, chat_id)\n")
 print('For any questions: discord K1rLes#3663')
