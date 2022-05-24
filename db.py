f6652b5efc140e2cbcee0a026ec0c1a2eb292530
K1rLes <104463209+K1rL3s@users.noreply.github.com>
2022-05-24 15:47:40 +0500

    add "with" context maganer

--- db.py
+++ db.py
@@ -8,24 +8,21 @@ REPORT_CHAT = 'chatId'  # paste the chat id where the bot will send reports
 
 
 def save_chat_in_db(chat_id, chat_name, chat_icon, chat_bg, chat_desc):
-    db = sqlite3.connect('database.db', check_same_thread=False)
-    sql = db.cursor()
-    chat_ids = tuple([i[0] for i in sql.execute("SELECT chat_id FROM chats")])
-    if chat_id in chat_ids:
-        sql.execute(f"UPDATE chats SET chat_id = '{chat_id}', chat_name = '{chat_name}', chat_icon = '{chat_icon}',"
-                    f"chat_bg = '{chat_bg}', chat_desc = '{chat_desc}' WHERE chat_id = '{chat_id}'")
-    else:
-        sql.execute(f"INSERT INTO chats VALUES ('{chat_id}', '{chat_name}', '{chat_icon}', '{chat_bg}', '{chat_desc}')")
-    db.commit()
-    db.close()
-    return True
+    with sqlite3.connect('database.db', check_same_thread=False) as db:
+        sql = db.cursor()
+        chat_ids = tuple([i[0] for i in sql.execute("SELECT chat_id FROM chats")])
+        if chat_id in chat_ids:
+            sql.execute(f"UPDATE chats SET chat_id = '{chat_id}', chat_name = '{chat_name}', chat_icon = '{chat_icon}',"
+                        f"chat_bg = '{chat_bg}', chat_desc = '{chat_desc}' WHERE chat_id = '{chat_id}'")
+        else:
+            sql.execute(f"INSERT INTO chats VALUES ('{chat_id}', '{chat_name}', '{chat_icon}', '{chat_bg}', '{chat_desc}')")
+        db.commit()
+        return True
 
 
 def return_chat_info_from_db(chat_id):
-    db = sqlite3.connect('database.db', check_same_thread=False)
-    sql = db.cursor()
-    s = sql.execute(f"SELECT * FROM chats WHERE chat_id = '{chat_id}'")
-    chat_info = s.fetchone()
-    db.close()
-    return chat_info
-
+    with sqlite3.connect('database.db', check_same_thread=False) as db:
+        sql = db.cursor()
+        s = sql.execute(f"SELECT * FROM chats WHERE chat_id = '{chat_id}'")
+        chat_info = s.fetchone()
+        return chat_info
