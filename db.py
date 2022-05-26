b47fdd1fff8c8660cc1b0e55570421663bb59262
K1rLes <104463209+K1rL3s@users.noreply.github.com>
2022-05-26 23:50:46 +0500

    add !allow !block

--- db.py
+++ db.py
@@ -25,3 +25,35 @@ def return_chat_info_from_db(chat_id):
         s = sql.execute(f"SELECT * FROM chats WHERE chat_id = '{chat_id}'")
         chat_info = s.fetchone()
         return chat_info
+
+   
+def blocked_commands(chat_id):
+    with sqlite3.connect('database.db', check_same_thread=False) as db:
+        sql = db.cursor()
+        commands = sql.execute(f"SELECT command FROM commands WHERE chat_id = '{chat_id}'").fetchone()[0]
+        return commands.strip()
+
+
+def block_command(chat_id, command):
+    with sqlite3.connect('database.db', check_same_thread=False) as db:
+        sql = db.cursor()
+        chat_ids = [i[0] for i in sql.execute("SELECT chat_id FROM commands")]
+        if chat_id in chat_ids:
+            commands = sql.execute(f"SELECT command FROM commands WHERE chat_id = '{chat_id}'").fetchone()[0].strip()
+            commands += ' ' + command
+            sql.execute(f"UPDATE commands SET command = ('{commands.strip()}') WHERE chat_id = '{chat_id}'")
+            db.commit()
+        else:
+            sql.execute(f"INSERT INTO commands VALUES ('{chat_id}', '{command.strip()}')")
+            db.commit()
+        return True
+
+
+def allow_command(chat_id, command):
+    with sqlite3.connect('database.db', check_same_thread=False) as db:
+        sql = db.cursor()
+        commands = sql.execute(f"SELECT command FROM commands WHERE chat_id = '{chat_id}'").fetchone()[0]
+        commands = commands.replace(command, '')
+        sql.execute(f"UPDATE commands SET command = ('{commands.strip()}') WHERE chat_id = '{chat_id}'")
+        db.commit()
+        return True
