10d874110f3d05cbda12d1c333857c08f4d9bf66
K1rLes <104463209+K1rL3s@users.noreply.github.com>
2022-05-26 23:53:26 +0500

    add table commands

--- init_db.py
+++ init_db.py
@@ -4,17 +4,22 @@ import sqlite3
 """
 START IT IF YOU DO NOT HAVE A 'database.db' FILE IN DIRECTORY
 OR DO NOT HAVE A 'TABLE chats'
+OR DO NOT HAVE A 'TABLE commands'
 """
 
+with sqlite3.connect('database.db') as db:
+    sql = db.cursor()
+    sql.execute("""
+        CREATE TABLE chats (
+        chat_id TEXT,
+        chat_name TEXT,
+        chat_icon TEXT,
+        chat_bg TEXT,
+        chat_desc TEXT)""")
+    db.commit()
 
-db = sqlite3.connect('database.db', check_same_thread=False)
-sql = db.cursor()
-sql.execute("""
-    CREATE TABLE chats (
-    chat_id TEXT,
-    chat_name TEXT,
-    chat_icon TEXT,
-    chat_bg TEXT,
-    chat_desc TEXT)""")
-db.commit()
-db.close()
+    sql.execute("""
+        CREATE TABLE commands (
+        chat_id TEXT,
+        command TEXT)""")
+    db.commit()
